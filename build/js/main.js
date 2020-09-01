49422da27af67ce1701539d3cf39537b9ba5a2a6
Hudson8811 <hudson88@yandex.ru>
2020-09-01 16:00:35 +0300

    add

--- build/js/main.js
+++ build/js/main.js
@@ -22,20 +22,21 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
 $(document).ready(function () {
 	
 });
-
 $(document).ready(function () {
 	$('.sh-burger').click(function () {
 		$(this).toggleClass('sh-burger--active');
 		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
+		$('body').toggleClass('body-menu-open');
 	});
+
 	$('.search-btn').click(function () {
 		$(this).closest('.search-wrap').toggleClass('search-wrap--active');
 		//$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
 	});
+
 	$('.site-menu-item--has-l2').click(function () {
-
-
 		$(this).siblings('.site-menu-l2').slideToggle(300);
+		$(this).toggleClass('site-menu-item--active-l2');
 		//$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
 	});
 
--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,17 +1,18 @@
-
 $(document).ready(function () {
 	$('.sh-burger').click(function () {
 		$(this).toggleClass('sh-burger--active');
 		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
+		$('body').toggleClass('body-menu-open');
 	});
+
 	$('.search-btn').click(function () {
 		$(this).closest('.search-wrap').toggleClass('search-wrap--active');
 		//$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
 	});
+
 	$('.site-menu-item--has-l2').click(function () {
-
-
 		$(this).siblings('.site-menu-l2').slideToggle(300);
+		$(this).toggleClass('site-menu-item--active-l2');
 		//$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
 	});
 
