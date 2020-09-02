739fed618ebe17bfe973eaa98e32a221401cc769
Hudson8811 <hudson88@yandex.ru>
2020-09-02 10:27:41 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@
 //= libs/jquery-3.3.1.min.js
 //= libs/jquery.fancybox.min.js
 //= libs/slick.min.js
+//= libs/jquery.nice-select.min.js
+//= libs/autosize.min.js
+
 
 /* my scripts */
 
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-   // $("input[name='phone']").mask(" +7 (999) 999-99-99");
+	// $("input[name='phone']").mask(" +7 (999) 999-99-99");
+	if ($('.js-autosize').length > 0) {
+		autosize($('.js-autosize'));
+	}
 
+	if ($('.js-fa-select').length > 0) {
+		$('.js-fa-select').niceSelect();
+	}
+	$('.js-custom-file-inp').on('change', function() {
+		var splittedFakePath = this.value.split('\\');
+		$(this).siblings('.tap-file-inp-text').addClass('tap-file-inp-text--non-empty').find('.tap-file-inp-text__name').text(splittedFakePath[splittedFakePath.length - 1]);
+  });
 });
-
